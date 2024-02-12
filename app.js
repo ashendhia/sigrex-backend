9bc124aadc3da489dc6511b2198d8a2969c8a595
wael-boudissa <waelb20102@gmail.com>
2024-02-12 01:11:46 +0100

    VersionP

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-const config = require('./utils/config')
-const express = require('express')
-require('express-async-errors')
-const app = express()
-const cors = require('cors')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const organizationRouter = require('./controllers/organization')
-const formateurRouter = require('./controllers/formateur/formateur')
+const config = require("./utils/config");
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const organizationRouter = require("./controllers/organization");
+const formateurRouter = require("./controllers/formateur/formateur");
 // Formateur Related
-const diplomeRouter = require('./controllers/formateur/diplome')
+const diplomeRouter = require("./controllers/formateur/diplome");
 // Formation Related
-const formationRouter = require('./controllers/formations/formation')
-const domaineRouter = require('./controllers/formations/domaine')
-const themeRouter = require('./controllers/formations/theme')
-const niveauRouter = require('./controllers/formations/niveau')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
+const competenceRouter = require("./controllers/formations/competence");
 
-logger.info('Connecting to planetscale database...')
+const formationRouter = require("./controllers/formations/formation");
+const domaineRouter = require("./controllers/formations/domaine");
+const themeRouter = require("./controllers/formations/theme");
+const niveauRouter = require("./controllers/formations/niveau");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
 
+logger.info("Connecting to planetscale database...");
 
+app.use(cors());
 
-app.use(cors())
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
-app.use('/api/organization', organizationRouter)
-app.use('/api/formateur', formateurRouter)
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/organization", organizationRouter);
+app.use("/api/formateur", formateurRouter);
 // Formateur Related
-app.use('/api/diplome', diplomeRouter)
+app.use("/api/diplome", diplomeRouter);
 // Formation Related
-app.use('/api/formation', formationRouter)
-app.use('/api/domaine', domaineRouter)
-app.use('/api/theme', themeRouter)
-app.use('/api/niveau', niveauRouter)
+app.use("/api/formation", formationRouter);
+app.use("/api/domaine", domaineRouter);
+app.use("/api/theme", themeRouter);
+app.use("/api/niveau", niveauRouter);
+// app.use("/api/competance", competenceRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
--- controllers/formateur/formateur.js
+++ controllers/formateur/formateur.js
@@ -1,99 +1,116 @@
-const formateurRouter = require('express').Router();
-const { adminExtractor } = require('../../utils/middleware.js');
-const bcrypt = require('bcrypt');
-const { prisma } = require('../../index.js'); // Adjust the path as needed
+const formateurRouter = require("express").Router();
+const { adminExtractor } = require("../../utils/middleware.js");
+const bcrypt = require("bcrypt");
+const { prisma } = require("../../index.js"); // Adjust the path as needed
 
-formateurRouter.post('/', async (request, response) => {
-    const { mail, name, photo, password, phone, address, more, familyName, sexe, diploma, employeur, fonction, cv, numeroCompte, status, salaire } = request.body;
+formateurRouter.post("/", async (request, response) => {
+  const {
+    mail,
+    name,
+    photo,
+    password,
+    phone,
+    address,
+    more,
+    familyName,
+    sexe,
+    diploma,
+    employeur,
+    fonction,
+    cv,
+    numeroCompte,
+    status,
+    salaire,
+  } = request.body;
 
-    const existingUser = await prisma.profile.findUnique({
-        where: {
-            mail: mail
-        }
-    })
+  const existingUser = await prisma.profile.findUnique({
+    where: {
+      mail: mail,
+    },
+  });
 
-    if (existingUser) {
-        return response.status(400).json({
-            error: 'Un seul compte par adresse email'
-        })
-    }
+  if (existingUser) {
+    return response.status(400).json({
+      error: "Un seul compte par adresse email",
+    });
+  } else if (password.length < 3) {
+    return response.status(400).json({
+      error: "password less than 3 characters",
+    });
+  }
 
-    else if (password.length < 3) {
-        return response.status(400).json({
-            error: 'password less than 3 characters'
-        })
-    }
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
+  const diplomaDB = await prisma.diploma.findUnique({
+    where: {
+      designation: diploma,
+    },
+  });
 
-    const diplomaDB = await prisma.diploma.findUnique({
-        where: {
-            designation: diploma
-        }
+  if (!diplomaDB) {
+    return response.status(400).json({
+      error: "diploma not found",
+    });
+  }
+
+  try {
+    const profile = await prisma.profile.create({
+      data: {
+        mail,
+        name,
+        photo,
+        password: passwordHash,
+        phone,
+        address,
+        more,
+        user: {
+          create: {
+            familyName,
+            sexe,
+            formateur: {
+              create: {
+                diplomaId: diplomaDB.id,
+                employeur,
+                fonction,
+                cv,
+                numeroCompte,
+                status,
+                salaire,
+              },
+            },
+          },
+        },
+      },
     });
 
-    if (!diplomaDB) {
-        return response.status(400).json({
-            error: 'diploma not found'
-        })
-    }
-
-
-
-    try {
-        const profile = await prisma.profile.create({
-            data: {
-                mail,
-                name,
-                photo,
-                password: passwordHash,
-                phone,
-                address,
-                more,
-                user: {
-                    create: {
-                        familyName,
-                        sexe,
-                        formateur: {
-                            create: {
-                                diplomaId: diplomaDB.id,
-                                employeur,
-                                fonction,
-                                cv,
-                                numeroCompte,
-                                status,
-                                salaire,
-                            }
-                        }
-                    }
-                }
-            }
-        });
-
-        response.status(201).json(profile);
-    } catch (error) {
-        response.status(500).json(error);
-    }
+    response.status(201).json(profile);
+  } catch (error) {
+    response.status(500).json(error);
+  }
 });
 
-formateurRouter.put('/:id', adminExtractor, async (request, response) => {
-    const { id, approved } = request.params;
+formateurRouter.put("/:id", adminExtractor, async (request, response) => {
+  const { id, approved } = request.params;
 
-    try {
-        const formateur = await prisma.formateur.update({
-            where: {
-                id: parseInt(id)
-            },
-            data: {
-                approved: approved
-            }
-        });
+  try {
+    const formateur = await prisma.formateur.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        approved: approved,
+      },
+    });
 
-        response.status(200).json(formateur);
-    } catch (error) {
-        response.status(500).json({ error: 'Something went wrong' });
-    }
-})
+    response.status(200).json(formateur);
+  } catch (error) {
+    response.status(500).json({ error: "Something went wrong" });
+  }
+});
+formateurRouter.get("/", async (request, response) => {
+  const formateur = await prisma.formateur.findMany();
+  response.json(formateur);
+});
 
 module.exports = formateurRouter;
